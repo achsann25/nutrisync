133935bf16e45d8a73a1a9c16a59bb1af5fd5cb0
A.Chasanuddin <achmad.10123367@mahasiswa.unikom.ac.id>
2026-02-10 14:23:26 +0700

    update composer to be more flexible

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nutrisync/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '77e76b6cc8f0a94ce2ff8a347364c83daccca3a9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nutrisync/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '77e76b6cc8f0a94ce2ff8a347364c83daccca3a9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
